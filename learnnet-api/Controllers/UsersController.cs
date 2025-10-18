9de9d0e8d850a6f11908c235d44bde19281fddf8
TUF_GAMING <jayashanchamaka@gmail.com>
2025-10-18 08:31:02 +0530

    Added the UsersController

---
+++ learnnet-api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learnnet_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(new { message = "GetUsers endpoint hit" });
+        }
+    }
+}
